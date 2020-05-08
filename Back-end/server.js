801f8988687be526688be805f7ff8af00afd0e43
Jguzman <juan@DESKTOP-DILVCG8.localdomain>
2020-05-08 17:16:01 -0400

    start of the back end

---
+++ Back-end/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req,res)=> res.json({msg: 'Welcome to the flash cards API...'}))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=> console.log(`server started on port ${PORT}`));
