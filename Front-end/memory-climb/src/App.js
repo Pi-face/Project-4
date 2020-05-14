e7d96a37d2f6470718b4f8fb9e63fdc77c05fc25
Jguzman <juan@DESKTOP-DILVCG8.localdomain>
2020-05-14 19:14:38 -0400

    Got the private route component working

--- Front-end/memory-climb/src/App.js
+++ Front-end/memory-climb/src/App.js
@@ -6,6 +6,7 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import FlashcardsState from './context/category/flashcardsState'
@@ -30,7 +31,7 @@ function App() {
            <div className='container'>
             <Alerts/>
             <Switch>
-              <Route exact path='/' component={Home}/>
+              <PrivateRoute exact path='/' component={Home}/>
               <Route exact path='/about' component={About}/>
               <Route exact path='/register' component={Register}/>
               <Route exact path='/login' component={Login}/>
